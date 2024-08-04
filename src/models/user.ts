5975e3c1779e10fbf9ae8d08f8e11f0a011b610c
Erlend <erlend.eelmets@gmail.com>
2024-08-04 13:40:20 +0300

    Remove unneeded file

--- src/models/user.ts
+++ src/models/user.ts
@@ -13,9 +13,9 @@ const userSchema = new Schema<IUser>({
 userSchema.set("toJSON", {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
-    delete returnedObject.passwordHash;
+    returnedObject._id = undefined;
+    returnedObject.__v = undefined;
+    returnedObject.passwordHash = undefined;
   },
 });
 
